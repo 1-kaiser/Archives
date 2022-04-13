f046aa3da4f9470a9a8ced880c9ee9942b564d7f
Soren <103299224+Soren1999@users.noreply.github.com>
2022-04-13 15:32:10 +0800

    Temperature Converter

---
+++ TempConverter.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int sum;
+int main() {
+			//Activity 1
+	float Fahrenheit = 90, Celsius;
+
+	Celsius = ( (Fahrenheit - 32) * 5/9);
+
+	clrscr();
+	printf("Celsius is: %.2f",Celsius);
+
+	getch();
+	return 0;
+}
+
+
+
+
